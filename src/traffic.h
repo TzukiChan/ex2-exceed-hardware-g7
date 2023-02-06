a8a0111691db1048685378b7174aeec9bbe6d087
TzukiChan <dosu_54@hotmail.com>
2023-02-06 15:25:19 +0700

    main

--- src/traffic.h
+++ src/traffic.h
@@ -6,8 +6,8 @@
 const String baseUrl = "https://exceed-hardware-stamp465.koyeb.app";
 
 const String point = "กลุ่มที่";
-const int nearby_1 = "กลุ่มใกล้เคียง (กลุ่มที่ +-1)";
-const int nearby_2 = "กลุ่มใกล้เคียง (กลุ่มที่ +-1)";
+const String nearby_1 = "กลุ่มใกล้เคียง (กลุ่มที่ +-1)";
+const String nearby_2 = "กลุ่มใกล้เคียง (กลุ่มที่ +-1)";
 
 void GET_traffic()
 {
