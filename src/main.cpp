08db2a63bb733e045b2ebe6a28eec3eda9ce9ec9
TzukiChan <dosu_54@hotmail.com>
2023-02-06 15:23:37 +0700

    main

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,13 @@
 #include <Bounce2.h>
 #include "traffic.h"
 
-#define red <led red pin>
-#define yellow <led yellow pin>
-#define green <led green pin>
-#define ldr <ldr pin>
-#define button <button pin>
+#define RED 26
+#define YELLOW 25
+#define GREEN 33
+#define LDR 34
+#define BUTTON 27
 
-#define light <แสดงมันมืด มีค่าเท่าไหร่>
+#define light analogRead(LDR)
 
 int state = 1;
 int count = 0;
@@ -23,42 +23,85 @@ void Connect_Wifi();
 void setup()
 {
   Serial.begin(115200);
-  pinMode(red, OUTPUT);
-  pinMode(yellow, OUTPUT);
-  pinMode(green, OUTPUT);
-  pinMode(ldr, INPUT);
-  debouncer.attach(button, INPUT_PULLUP);
+  pinMode(RED, OUTPUT);
+  pinMode(YELLOW, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(LDR, INPUT);
+  debouncer.attach(BUTTON, INPUT_PULLUP);
   debouncer.interval(25);
   Connect_Wifi();
 
   delay(200);
   // start LED with GREEN and POST to database
-  digitalWrite(green, HIGH);
+  digitalWrite(GREEN, 1);
+  // Serial.println("green na");
   POST_traffic("green");
+  GET_traffic();
+  digitalWrite(YELLOW, 0);
+  digitalWrite(RED, 0);
 }
 
 void loop()
 {
   // *** write your code here ***
   // Your can change everything that you want
+
   if (state == 1)
   {
     // while led GREEN
+    if (count == 0)
+    {
+      debouncer.update();
+      if (debouncer.fell()) 
+      { 
+        count++;
+        Serial.println(count);
+      }
+    }
+    else if (count == 1)
+    {
+      digitalWrite(GREEN, 0);
+      digitalWrite(YELLOW, 1);
+      digitalWrite(RED, 0);
+      // Serial.println("yellow na");
+      POST_traffic("yellow");
+      state += 1;
+      count = 0;
+    }
   }
   else if (state == 2)
   {
     // while led YELLOW
+    delay (8000);
+    digitalWrite(GREEN, 0);
+    digitalWrite(YELLOW, 0);
+    digitalWrite(RED, 1);
+    // Serial.println("red na");
+    POST_traffic("red");
+    GET_traffic();
+    state += 1;    
   }
   else if (state == 3)
   {
     // while led RED
+    delay (5000);
+    if (light < 3000)
+    {
+      digitalWrite(GREEN, 1);
+      digitalWrite(YELLOW, 0);
+      digitalWrite(RED, 0);
+      // Serial.println("green na");
+      POST_traffic("green");
+      GET_traffic();
+      state = 1;
+    }
   }
 }
 
 void Connect_Wifi()
 {
-  const char *ssid = "Your Wifi Name";
-  const char *password = "Your Wifi Password";
+  const char *ssid = "nitro 5";
+  const char *password = "12345687";
   WiFi.begin(ssid, password);
   Serial.print("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED)
